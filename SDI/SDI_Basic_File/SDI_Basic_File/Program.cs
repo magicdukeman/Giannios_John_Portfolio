7e3045847ef246793be9e54b5923389afb0a820f
magicdukeman <magicdukeman@fullsail.edu>
2016-01-24 17:10:23 -0500

    SDI

---
+++ SDI/SDI_Basic_File/SDI_Basic_File/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SDI_Basic_File
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Lets get started programing!");
+			Console.WriteLine ("This is going to be so fun!");
+		}
+	}
+}
